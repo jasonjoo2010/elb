04f99f122ae93479e0f30fb00daaa83edb08a276
jason <jason@dayima.com>
2019-05-21 00:54:52 +0800

    the rest

--- waf/init.lua
+++ waf/init.lua
@@ -8,9 +8,9 @@ local get_headers = ngx.req.get_headers
 local ModuleEnable = function(options) return options == "true" and true or false end
 logpath = configLogDir
 rulepath = configRulePath
-UrlDeny = ModuleEnable(configUrlDeny)
-PostCheck = ModuleEnable(configPost)
-CookieCheck = ModuleEnable(cookieMatch)
+enableUrlCheck = ModuleEnable(configUrlDeny)
+enablePostCheck = ModuleEnable(configPost)
+enableCookieCheck = ModuleEnable(configCookieMatch)
 enableURLWhite = ModuleEnable(configURLWhite)
 PathInfoFix = ModuleEnable(PathInfoFix)
 attacklog = ModuleEnable(configAttackLog)
@@ -89,15 +89,16 @@ local function waf_log(method, url, data, ruletag)
     if attacklog then
         local realIp = waf_get_client_ip()
         local ua = ngx.var.http_user_agent
-        local servername=ngx.var.server_name
-        local time=ngx.localtime()
+        local servername = ngx.var.server_name
+        local time = ngx.localtime()
+        local line
         if ua  then
             line = realIp.." ["..time.."] \""..method.." "..servername..url.."\" \""..data.."\"  \""..ua.."\" \""..ruletag.."\"\n"
         else
             line = realIp.." ["..time.."] \""..method.." "..servername..url.."\" \""..data.."\" - \""..ruletag.."\"\n"
         end
-        local filename = logpath..'/'..servername.."_"..ngx.today().."_sec.log"
-        write(filename,line)
+        local filename = logpath .. '/' .. servername .. "_" .. ngx.today() .. "_sec.log"
+        write(filename, line)
     end
 end
 ------------------------------------load rules-------------------------------------------------------------------
@@ -133,11 +134,11 @@ function say_html()
     end
 end
 
-function whiteurl()
+function waf_white_url()
     if enableURLWhite then
-        if wturlrules ~=nil then
-            for _,rule in pairs(wturlrules) do
-                if ngxmatch(ngx.var.uri,rule,"isjo") then
+        if wturlrules ~= nil then
+            for _, rule in pairs(wturlrules) do
+                if ngxmatch(ngx.var.uri, rule, "isjo") then
                     return true
                 end
             end
@@ -159,21 +160,22 @@ function fileExtCheck(ext)
     end
     return false
 end
+
 function Set (list)
     local set = {}
     for _, l in ipairs(list) do set[l] = true end
     return set
 end
-function args()
-    for _,rule in pairs(argsrules) do
+
+function waf_check_args()
+    for _, rule in pairs(argsrules) do
         local args = ngx.req.get_uri_args()
         for key, val in pairs(args) do
-            if type(val)=='table' then
+            local data = val
+            if type(val) == 'table' then
                 if val ~= false then
                     data = table.concat(val, " ")
                 end
-            else
-                data = val
             end
             if data and type(data) ~= "boolean" and rule ~= "" and ngxmatch(unescape(data), rule, "isjo") then
                 waf_log('GET', ngx.var.request_uri, "-", rule)
@@ -185,12 +187,11 @@ function args()
     return false
 end
 
-
-function url()
-    if UrlDeny then
-        for _,rule in pairs(urlrules) do
-            if rule ~="" and ngxmatch(ngx.var.request_uri, rule, "isjo") then
-                waf_log('GET',ngx.var.request_uri,"-",rule)
+function waf_check_url()
+    if enableUrlCheck then
+        for _, rule in pairs(urlrules) do
+            if rule ~= "" and ngxmatch(ngx.var.request_uri, rule, "isjo") then
+                waf_log('GET', ngx.var.request_uri, "-", rule)
                 say_html()
                 return true
             end
@@ -199,12 +200,12 @@ function url()
     return false
 end
 
-function ua()
+function waf_check_ua()
     local ua = ngx.var.http_user_agent
     if ua ~= nil then
-        for _,rule in pairs(uarules) do
-            if rule ~="" and ngxmatch(ua,rule,"isjo") then
-                waf_log('UA',ngx.var.request_uri,"-",rule)
+        for _, rule in pairs(uarules) do
+            if rule ~= "" and ngxmatch(ua, rule, "isjo") then
+                waf_log('UA', ngx.var.request_uri, "-", rule)
                 say_html()
                 return true
             end
@@ -222,12 +223,13 @@ function body(data)
     end
     return false
 end
-function cookie()
+function waf_check_cookie()
+    if not enableCookieCheck then return false end
     local ck = ngx.var.http_cookie
-    if CookieCheck and ck then
-        for _,rule in pairs(ckrules) do
-            if rule ~="" and ngxmatch(ck,rule,"isjo") then
-                waf_log('Cookie',ngx.var.request_uri,"-",rule)
+    if ck then
+        for _, rule in pairs(ckrules) do
+            if rule ~= "" and ngxmatch(ck, rule, "isjo") then
+                waf_log('Cookie', ngx.var.request_uri, "-", rule)
                 say_html()
                 return true
             end
@@ -256,7 +258,7 @@ function waf_deny_cc()
     return false
 end
 
-function get_boundary()
+local function get_boundary()
     local header = get_headers()["content-type"]
     if not header then
         return nil
@@ -288,74 +290,73 @@ function waf_block_ip()
     return false
 end
 
-function post_check()
-    if not PostCheck then return false end
+function waf_check_post()
+    if not enablePostCheck then return false end
     local method = ngx.req.get_method()
-    if method == "POST" then
-        local boundary = get_boundary()
-        if boundary then
-            local len = string.len
-            local sock, err = ngx.req.socket()
-            if not sock then
+    if method ~= "POST" then return false end
+    local boundary = get_boundary()
+    if boundary then
+        local len = string.len
+        local sock, err = ngx.req.socket()
+        if not sock then
+            return
+        end
+        ngx.req.init_body(128 * 1024)
+        sock:settimeout(0)
+        local content_length = nil
+        content_length = tonumber(ngx.req.get_headers()['content-length'])
+        local chunk_size = 4096
+        if content_length < chunk_size then
+            chunk_size = content_length
+        end
+        local size = 0
+        while size < content_length do
+            local data, err, partial = sock:receive(chunk_size)
+            data = data or partial
+            if not data then
                 return
             end
-            ngx.req.init_body(128 * 1024)
-            sock:settimeout(0)
-            local content_length = nil
-            content_length = tonumber(ngx.req.get_headers()['content-length'])
-            local chunk_size = 4096
-            if content_length < chunk_size then
-                chunk_size = content_length
+            ngx.req.append_body(data)
+            if body(data) then
+                return true
             end
-            local size = 0
-            while size < content_length do
-                local data, err, partial = sock:receive(chunk_size)
-                data = data or partial
-                if not data then
+            size = size + len(data)
+            local m = ngx.re.match(data, [[Content-Disposition: form-data;(.+)filename="(.+)\\.(.*)"]],'ijo')
+            if m then
+                fileExtCheck(m[3])
+                filetranslate = true
+            else
+                if ngxmatch(data, "Content-Disposition:", 'isjo') then
+                    filetranslate = false
+                end
+                if filetranslate == false then
+                    if body(data) then
+                        return true
+                    end
+                end
+            end
+            local less = content_length - size
+            if less < chunk_size then
+                chunk_size = less
+            end
+        end
+        ngx.req.finish_body()
+    else
+        ngx.req.read_body()
+        local args = ngx.req.get_post_args()
+        if not args then
+            return
+        end
+        for key, val in pairs(args) do
+            local data = val
+            if type(val) == "table" then
+                if type(val[1]) == "boolean" then
                     return
                 end
-                ngx.req.append_body(data)
-                if body(data) then
-                    return true
-                end
-                size = size + len(data)
-                local m = ngxmatch(data, [[Content-Disposition: form-data;(.+)filename="(.+)\\.(.*)"]],'ijo')
-                if m then
-                    fileExtCheck(m[3])
-                    filetranslate = true
-                else
-                    if ngxmatch(data, "Content-Disposition:",'isjo') then
-                        filetranslate = false
-                    end
-                    if filetranslate == false then
-                        if body(data) then
-                            return true
-                        end
-                    end
-                end
-                local less = content_length - size
-                if less < chunk_size then
-                    chunk_size = less
-                end
+                data = table.concat(val, ", ")
             end
-            ngx.req.finish_body()
-        else
-            ngx.req.read_body()
-            local args = ngx.req.get_post_args()
-            if not args then
-                return
-            end
-            for key, val in pairs(args) do
-                local data = val
-                if type(val) == "table" then
-                    if type(val[1]) == "boolean" then
-                        return
-                    end
-                    data = table.concat(val, ", ")
-                end
-                if data and type(data) ~= "boolean" and body(data) then
-                    return true
-                end
+            if data and type(data) ~= "boolean" and body(data) then
+                return true
             end
         end
     end
--- waf/waf.lua
+++ waf/waf.lua
@@ -1,17 +1,17 @@
 local function waf_filter()
-    if waf_white_ip() then
-    elseif waf_block_ip() then
-    elseif waf_deny_cc() then
+    if waf_white_ip() then -- need to be dynamic
+    elseif waf_block_ip() then -- need to be dynamic
+    elseif waf_deny_cc() then -- need to be per domain ?
     elseif ngx.var.http_Acunetix_Aspect then
-        ngx.exit(444)
+        ngx.exit(409)
     elseif ngx.var.http_X_Scan_Memo then
-        ngx.exit(444)
-    elseif whiteurl() then
-    elseif ua() then
-    elseif url() then
-    elseif args() then
-    elseif cookie() then
-    elseif post_check() then
+        ngx.exit(409)
+    elseif waf_white_url() then -- need to be per domain ?
+    elseif waf_check_ua() then
+    elseif waf_check_url() then
+    elseif waf_check_args() then
+    elseif waf_check_cookie() then
+    elseif waf_check_post() then
     else
         return
     end
